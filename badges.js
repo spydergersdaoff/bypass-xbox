d920ec5ecd20b0f581ed900d1bae29cb7424dd83
SpyderGersda <160939622+spydergersdaoff@users.noreply.github.com>
2026-01-02 13:34:48 +0100

    Update badge titles and streamline action function

--- badges.js
+++ badges.js
@@ -1,18 +1,15 @@
 const badgesConfig = {
     "has2026": {
         "img": "2026.png",
-        "title": "2026",
+        "title": "Badge 2026",
         "isStaffOnly": false
     },
     "isStaff": {
         "img": "admin.jpg",
         "title": "Staff",
         "isStaffOnly": true,
-        "action": () => {
-            document.getElementById('adminPanel').style.display = 'flex';
-        }
+        "action": () => { document.getElementById('adminPanel').style.display = 'flex'; }
     },
-    // TON NOUVEAU BADGE FRIEND
     "isFriend": {
         "img": "Friend.jpg",
         "title": "Ami Officiel",
